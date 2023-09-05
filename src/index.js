e38b2d6e5712408d702e98ee3d2f4e481c357c14
Raze100 <rawatrohit084@gmail.com>
2023-09-06 00:00:02 +0530

    First Commit with basic setup

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const { PORT } = require('./config/serverConfig')
+
+const apiRoutes = require('./routes/index')
+const db = require('./models/index')
+
+
+
+const prepareAndStartServer = () => {
+    
+    //here we are setting body-Parser as a middleware
+    //this wiil help us to read the requested body properly 
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api',apiRoutes);
+    app.listen(PORT,async()=>{
+        console.log(`Server started on PORT: ${PORT}`);
+
+        if(process.nextTick.DB_SYNC){
+            db.sequelize.sync({alter: true});
+        }
+    })
+}
+
+prepareAndStartServer();
